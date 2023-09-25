abb17cdf2661572f56842d8d409c011eeee3c42f
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-25 13:37:42 +0200

    Removed unused import

--- src/main/java/com/learnprojects/learnrestapi/helloworld/HelloWorldResource.java
+++ src/main/java/com/learnprojects/learnrestapi/helloworld/HelloWorldResource.java
@@ -3,7 +3,6 @@ package com.learnprojects.learnrestapi.helloworld;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
