fd8df35174a62bf9136029dc8ccdb1eeab9c0d17
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-25 15:12:56 +0200

    Added addNewSurveyQuestion

--- src/main/java/com/learnprojects/learnrestapi/survey/SurveyResource.java
+++ src/main/java/com/learnprojects/learnrestapi/survey/SurveyResource.java
@@ -1,11 +1,12 @@
 package com.learnprojects.learnrestapi.survey;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,7 +31,7 @@ public class SurveyResource {
     }
 
     @RequestMapping("/surveys/{surveyId}/questions")
-    public List<Question> retrieveAllSurveyQuestions(@PathVariable String surveyId) {
+    public List<Question> retrieveSurveyQuestionById(@PathVariable String surveyId) {
         List<Question> questions = surveyService.retrieveAllSurveyQuestions(surveyId);
 
         if (questions == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -39,12 +40,30 @@ public class SurveyResource {
     }
 
     @RequestMapping("/surveys/{surveyId}/questions/{questionId}")
-    public Question retrieveAllSurveyQuestions(@PathVariable String surveyId,
-                                                     @PathVariable String questionId) {
+    public Question retrieveSurveyQuestionById(@PathVariable String surveyId,
+                                               @PathVariable String questionId) {
         Question question = surveyService.retrieveSurveyQuestionById(surveyId, questionId);
 
         if (question == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         return question;
+    }
+
+    @RequestMapping(value= "/surveys/{surveyId}/questions", method = RequestMethod.POST)
+    public ResponseEntity<Object> addNewSurveyQuestion(@PathVariable String surveyId,
+                                                        @RequestBody Question question) {
+        String questionId = surveyService.addNewSurveyQuestion(surveyId, question);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{questionId}").buildAndExpand(questionId).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @RequestMapping(value = "/surveys/{surveyId}/questions/{questionId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteSurveyQuestionById(@PathVariable String surveyId,
+                                               @PathVariable String questionId) {
+        surveyService.deleteSurveyQuestionById(surveyId, questionId);
+
+        return ResponseEntity.noContent().build();
     }
 }
