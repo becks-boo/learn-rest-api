18cadfeb17099fba3633915d071e8d685b006765
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-26 11:04:47 +0200

    Created file for unit test and test for 404 scenario

---
+++ src/test/java/com/learnprojects/learnrestapi/survey/SurveyResourceTest.java
@@ -0,0 +1,31 @@
+package com.learnprojects.learnrestapi.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(controllers = SurveyResource.class)
+public class SurveyResourceTest {
+    @MockBean
+    private SurveyService surveyService;
+    @Autowired
+    private MockMvc mockMvc;
+    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+
+    @Test
+    void retrieveSpecificSurveyQuestion_404Scenario() throws Exception {
+        RequestBuilder requestBuilder =
+                MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+        System.out.println(mvcResult.getResponse().getStatus());
+    }
+}
